66612810c7935fa92bcc8c601a96085293d08cfe
LiGuo <liguo1976@outlook.com>
2020-07-09 17:29:00 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,7 @@ if [ -z "$INPUT_ENDPOINTID" ]; then
 fi
 
 #auth
+echo
 echo 'get token  : '${INPUT_SERVERURL}'/api/auth'
 Token_Result=$(curl --location --request POST ''${INPUT_SERVERURL}'/api/auth' \
 --data-raw '{"Username":"'$INPUT_USERNAME'", "Password":"'$INPUT_PASSWORD'"}')
@@ -15,6 +16,7 @@ Token_Result=$(curl --location --request POST ''${INPUT_SERVERURL}'/api/auth' \
 #todo: get token failed  exit 1
 token=$(echo $Token_Result | jq -r '.jwt')
 #get stacks
+echo
 echo 'get statcks :  '${INPUT_SERVERURL}'/api/stacks'
 stacks=$(curl --location --request GET ''${INPUT_SERVERURL}'/api/stacks' \
 --header 'Authorization: Bearer '$token'')
@@ -24,6 +26,7 @@ if [ $length > 0 ]; then
   stackId=$(echo $stacks | jq '.[] | select(.Name=="'$INPUT_STACKNAME'") | .Id')
   if [ $stackId > 0 ]; then
  #find the stack id, and delete it
+    echo
     echo 'delete stack id='$stackId'  '${INPUT_SERVERURL}'/api/stacks/'${stackId}' '
     curl --location --request DELETE ''${INPUT_SERVERURL}'/api/stacks/'${stackId}'' --header 'Authorization: Bearer '$token''
   fi
@@ -31,15 +34,21 @@ fi
 
 #create stacks
 compose=$(echo "$INPUT_DOCKER_COMPOSE" | sed 's#\"#\\"#g' | sed ":a;N;s/\\n/\\\\n/g;ta") # replace charactor  "->\"   \n -> \\n
+echo
 echo 'create stack  : '${INPUT_SERVERURL}'/api/stacks?endpointId='$INPUT_ENDPOINTID'&method=string&type=2'
+
+echo
+echo "{\"Name\":\"'${INPUT_STACKNAME}'\",\"StackFileContent\":\"'${compose}'\",\"Env\":[]}"
+
 result=$(curl --location --request POST ''${INPUT_SERVERURL}'/api/stacks?endpointId='$INPUT_ENDPOINTID'&method=string&type=2' \
 --header 'Authorization: Bearer '${token}'' \
 --header 'Content-Type: application/json' \
 --data-raw "{\"Name\":\"'${INPUT_STACKNAME}'\",\"StackFileContent\":\"'${compose}'\",\"Env\":[]}")
 echo "$result"
+echo
 message=$(echo $result | jq -r '.message')
 if [ -n "$message" ]; then
-  echo 'create failed:'$message''
+  echo 'create failed:    '$message''
   exit 1
 fi
 exit 0
