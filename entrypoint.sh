609dba1ab85993722be90a7fb383221f20972fd6
LiGuo <liguo1976@outlook.com>
2020-07-10 09:20:26 +0800

    添加测试输出

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,6 +24,7 @@ stacks=$(curl --location --request GET ''${INPUT_SERVERURL}'/api/stacks' \
 length=$(echo $stacks | jq '.|length')
 if [ $length > 0 ]; then
 #find the stack name of INPUT_STACKNAME
+  echo "$stackId"
   stackId=$(echo $stacks | jq '.[] | select(.Name=="'$stack'") | .Id')
   if [ $stackId > 0 ]; then
  #find the stack id, and delete it
