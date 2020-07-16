88307e4a8b0aad3c44f091c357a84c5f091ce63d
LiGuo <liguo1976@outlook.com>
2020-07-16 10:04:47 +0800

    add comment

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,9 +7,10 @@ if [ -z "$INPUT_ENDPOINTID" ]; then
  $INPUT_ENDPOINTID=1
 fi
 
+#把stack name转为小写
 stack=$(echo "$INPUT_STACKNAME" | tr 'A-Z' 'a-z') #ToLowerCase
-#auth
-echo
+
+#请求/api/auth 进行身份验证  获取token
 echo 'get token  : '${INPUT_SERVERURL}'/api/auth'
 Token_Result=$(curl --location --request POST ''${INPUT_SERVERURL}'/api/auth' \
 --data-raw '{"Username":"'$INPUT_USERNAME'", "Password":"'$INPUT_PASSWORD'"}')
@@ -19,43 +20,50 @@ token=$(echo $Token_Result | jq -r '.jwt')
 #get stacks
 echo
 echo 'get statcks :  '${INPUT_SERVERURL}'/api/stacks'
+#请求/api/stacks 查询stack列表
 stacks=$(curl --location --request GET ''${INPUT_SERVERURL}'/api/stacks' \
 --header 'Authorization: Bearer '$token'')
 echo "stacks: $stacks"
+#获取stack列表长度，如果为空则长度为0
 length=$(echo $stacks | jq '.|length')
 echo "length: $length"
+#如果长度大于0
 if [ length > 0  ]; then
-#find the stack name of INPUT_STACKNAME
-  stackId=$(echo $stacks | jq '.[] | select(.Name=="'$stack'") | .Id')
+  #查找同名stack
+  stackId=$(echo $stacks | jq '.[] | select(.Name=="'$stack'") | .Id') #find the stack name of INPUT_STACKNAME
   echo "stackId: $stackId"
   if [ stackId > 0 ]; then
  #find the stack id, and delete it
     echo
     echo 'delete stack id='$stackId'  '${INPUT_SERVERURL}'/api/stacks/'${stackId}''
+    #找到同名stack，通过stackId进行删除
     curl --location --request DELETE ''${INPUT_SERVERURL}'/api/stacks/'${stackId}'' --header 'Authorization: Bearer '$token''
   fi
 fi
 
 echo 'pull image: '$INPUT_IMAGENAME''
-#pull image
+#pull image  
+#拉取镜像
 curl --location --request POST ''${INPUT_SERVERURL}'/api/endpoints/'$INPUT_ENDPOINTID'/docker/images/create?fromImage='$INPUT_IMAGENAME'' \
 -H 'Authorization: Bearer '${token}''
 
 
 #create stacks
+#创建stack
 compose=$(echo "$INPUT_DOCKER_COMPOSE" | sed 's#\"#\\"#g' | sed ":a;N;s/\\n/\\\\n/g;ta") # replace charactor  "->\"   \n -> \\n
 echo
 echo 'create stack  : '${INPUT_SERVERURL}'/api/stacks?endpointId='$INPUT_ENDPOINTID'&method=string&type=2'
 
 echo
 #echo "{\"Name\":\"'${INPUT_STACKNAME}'\",\"StackFileContent\":\"${compose}\",\"Env\":[]}"
-
+#输出结果
 result=$(curl --location --request POST ''${INPUT_SERVERURL}'/api/stacks?endpointId='$INPUT_ENDPOINTID'&method=string&type=2' \
 --header 'Authorization: Bearer '${token}'' \
 --header 'Content-Type: application/json' \
 --data-raw "{\"Name\":\"'${stack}'\",\"StackFileContent\":\"${compose}\",\"Env\":[]}")
 echo "$result"
 echo
+#如果结果中message不为空则说明有异常
 message=$(echo $result | jq -r '.message')
 if [ $message != 'null' ]; then
   echo "create failed: $message"
