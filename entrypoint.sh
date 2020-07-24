02a89e301bd7d34e8b184c96ff7db0014e4079cd
LiGuo <liguo1976@outlook.com>
2020-07-24 10:38:29 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -30,9 +30,9 @@ fi
 
 #pull image  
 #拉取镜像
-echo "pull image: $INPUT_IMAGENAME"
-curl --location --request POST ''${INPUT_SERVERURL}'/api/endpoints/'$INPUT_ENDPOINTID'/docker/images/create?fromImage='$INPUT_IMAGENAME'' \
--H 'Authorization: Bearer '${token}''
+# echo "pull image: $INPUT_IMAGENAME"
+# curl --location --request POST ''${INPUT_SERVERURL}'/api/endpoints/'$INPUT_ENDPOINTID'/docker/images/create?fromImage='$INPUT_IMAGENAME'' \
+# -H 'Authorization: Bearer '${token}''
 
 
 
@@ -56,8 +56,8 @@ if [ $length -gt 0  ]; then
     echo
     echo "update stack id=$stackId"
     #找到同名stack，更新stack
-    update_content=$(jq -n -c -M --arg content "$compose" --arg id $stackId '{"id": $id, "StackFileContent": "'$content'}"')
-    update_result=$(curl --location --request PUT ''${INPUT_SERVERURL}'/api/stacks/'${stackId}?endpointId=${INPUT_ENDPOINTID}'' --header 'Authorization: Bearer '$token'' --data-raw "$update_content")
+    update_content=$(jq -n -c -M --arg content "$compose" --arg id $stackId '{"id": $id, "StackFileContent": $content}')
+    update_result=$(curl --location --request PUT ''${INPUT_SERVERURL}'/api/stacks/'${stackId}?endpointId=${INPUT_ENDPOINTID}'' --header 'Authorization: Bearer '$token'' --data-raw $update_content)
     update_result_msg=$(echo "$update_result" | jq -r '.message')
     if [ "$update_result_msg" != "null" ]; then
       echo "update stack failed"
