bef9fc2cd11a1459dfed4238313e18c8dc26f892
LiGuo <liguo1976@outlook.com>
2020-07-24 11:09:56 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -58,7 +58,7 @@ if [ $length -gt 0  ]; then
     echo "update stack id=$stackId"
     #找到同名stack，更新stack
     # update_content=$(jq -n -c -M --arg content "$compose" --arg id $stackId '{"id": $id, "StackFileContent": $content}')
-    update_content="{\"id\":${stackId},\"StackFileContent\":\"${compose}\",\"Env\":[]}"
+    update_content="{\"id\":${stackId},\"StackFileContent\":\"${INPUT_DOCKER_COMPOSE}\",\"Env\":[]}"
     update_result=$(curl --location --request PUT ''${INPUT_SERVERURL}'/api/stacks/'${stackId}?endpointId=${INPUT_ENDPOINTID}'' \
      --header 'Authorization: Bearer '$token'' \
      --data-raw "$update_content")
