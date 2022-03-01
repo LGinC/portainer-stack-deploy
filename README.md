84fb3d5279dd2aa1873478181b16e0e56781223a
李果 <liguo1976@outlook.com>
2022-03-01 16:16:54 +0800

    change to rust

--- README.md
+++ README.md
@@ -8,12 +8,18 @@ This action deploy image by portainer
 portainer服务器公网地址  需要公网能够访问
 
 ## username
-**required**  portainer username <br>
+portainer username <br>
 portainer 面板登录所用的用户名
 
 ## password
-**required**  portainer password <br>
+portainer password <br>
 portainer 面板登录所用的密码
+
+## access_token
+portainer account [access token](https://docs.portainer.io/v/ce-2.11/api/access). login by username password or access_token.<br> 
+click on my account in the top right -> Scroll down to the Access tokens section -> click the Add access token.<br>
+可选 portainer账户的 [访问令牌](https://docs.portainer.io/v/ce-2.11/api/access), 登录方式二选一 用户名-密码 或者 访问令牌. <br>
+创建访问令牌在 portainer 右上角 myAccount, 下拉到Access Token, 点击 Add access token,输入描述 确定
 
 ## endpointId
 portainer endpoint id,default 1,  localhost is 1 <br>
@@ -23,31 +29,50 @@ portainer终结点id，默认是1,即第一个，一般为localhost
 **required** name of stack <br>
 服务栈的名称，会在stacks列表里显示
 
-## registry
-address of registry, like: myregistry.example.com (leave empty for Docker Hub) <br>
+## imagenames
+names of pull images, a arrary. add this param because not auto pull image when image:tag not change in docker-compose <br>
+可选 将会进行拉取镜像的镜像名列表, 为数组.加这个参数是因为docker-compose里的镜像名:tag 没有变化则不会自动拉取镜像
 
-## imagename
-**required** name of pull image, like:  mcr.microsoft.com/dotnet/core/aspnet:3.1-alpine <br>
-将会进行拉取镜像的镜像名
+## env
+environments of stack <br>
+可选 环境变量列表 
+![env](https://p.sda1.dev/5/b982dedaf195db23d1767701e4200ebd/msedge_xwrxILQuNN.webp)
+
+## variables
+these variables will be replaced in docker-compose file，  Foo=bar will replace {{ Foo }} for bar.<br>
+变量列表，会自动替换docker-compose中的变量 如设定Foo=bar,  则会把docker-compose中的{{ Foo }}替换为bar
 
 ## docker_compose
-content of docker-compose.yml.  ps: portainer just support version: "2" <br>
-docker-compose.yml的内容 注意:portainer目前仅支持version: "2" <br>
-like this:
+content of docker-compose.yml.  it will be filled by original stack when stack exist. <br>
+docker-compose.yml的内容 如果stack已经存在,不填则会自动获取已经存在的stack内容 <br>
+sample like this:
 ```
 docker_compose: |
-  version: "2"
+  version: "3"
   services:
-  dotnet_test:
-    image:  mcr.microsoft.com/dotnet/core/aspnet:3.1-alpine
-    container_name: dotnet_runtime
+    dotnettest:
+      image:  mcr.microsoft.com/dotnet/core/aspnet:6.0-alpine
+      container_name: dotnet_runtime
 ```
-leave empty for use the current docker-compose file content
 <br>
+
+## docker_compose_path
+docker-compose.yml in repository relative path. just need choose one between docker_compose and docker_compose_path.
+note: if stack exist, it will be use original content of stack, not content of docker-compose.yml in repository, so docker_compose_path can work when stack not exist <br>
+可选, docker-comose.yml在git仓库中的相对路径, docker_compose和docker_compose_path二选一即可, 注意,如果stack已存在会先使用已存在的stack的内容,即docker_compose_path仅能用于创建stack的情况
     
+## repo_username
+username of git repository<br>
+可选, git仓库用户名
+
+## repo_password
+password of git repository<br>
+可选, git仓库密码
 # Example usage
 The following will pull image mcr.microsoft.com/dotnet/core/aspnet:3.1-alpine, and deploy docker-compose to portainer.</br>
-```
+
+docker-compose in step
+```yaml
 - name: deploy to portainer
   uses: LGinC/portainer-stack-deploy@master
   with: 
@@ -56,12 +81,39 @@ The following will pull image mcr.microsoft.com/dotnet/core/aspnet:3.1-alpine, a
     password: ${{ secrets.PORTAINER_PASSWORD }}
     endpointId: 1
     stackname: dotnet_test
-    registry: mcr.microsoft.com
-    imagename: dotnet/core/aspnet:3.1-alpine
+    imagenames: 
+      - xxx/xxx
+      - myhub.com/xx1/xxx
+    variables:
+      - tag=6.0-alpine
+    env:
+      - TZ:Asia/Shanghai
+      - myTag:App
     docker_compose: |
-      version: "2"
+      version: "3"
       services:
         dotnet_test:
-          image:  mcr.microsoft.com/dotnet/core/aspnet:3.1-alpine
+          image:  mcr.microsoft.com/dotnet/core/aspnet:{{ tag }}
           container_name: dotnet_runtime       
+```
+
+or
+
+<br>docker-compose in repository
+
+```yaml
+- name: portainer
+  uses: LGinC/portainer-stack-deploy@master
+  with:
+    serverurl: http://xxxxx:9000
+    access_token:  ${{ secrets.PORTAINER_AK }}
+    endpointId: 2
+    stackname: xxxservice
+    imagenames: 
+      - xxx/xxx
+    env:
+      - myTag:App
+    docker_compose_path: deploy/docker-compose.yaml
+    repo_username:  ${{ secrets.REPO_USERNAME }}
+    repo_password:  ${{ secrets.REPO_PASSWORD }}
 ```
---
+++ src/main.rs
@@ -0,0 +1,270 @@
+use base64::encode;
+use serde::Serialize;
+use std::collections::HashMap;
+use std::env;
+use std::string::String;
+#[derive(Debug, Serialize)]
+pub struct Pair {
+    name: String,
+    value: String,
+}
+
+fn get_pair_from_env(env: &str) -> Vec<Pair> {
+    let env_str = match env::var(env) {
+        Ok(e) => e,
+        Err(_) => String::default(),
+    };
+    let envs: Vec<&str> = match env_str.as_str() {
+        "" => Vec::new(),
+        v => v.split(',').collect(),
+    };
+    let mut re = Vec::<Pair>::new();
+    if envs.len() > 0 {
+        for e in envs {
+            let ep: Vec<&str> = e.split('=').into_iter().collect();
+            re.push(Pair {
+                name: ep[0].trim().to_string(),
+                value: ep[1].trim().to_string(),
+            })
+        }
+    }
+    re
+}
+
+fn get_env_string(env: &str, default: Option<String>) -> String {
+    match env::var(env) {
+        Ok(e) => e,
+        Err(_) => default.unwrap_or_default(),
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
+    //portainer server url
+    let server = env::var("INPUT_SERVERURL").unwrap();
+    //portainer endpoint, default 1
+    let endpoint = get_env_string("INPUT_ENDPOINTID", Some(String::from("1")));
+    //stack content, content of docker-compose.yml
+    let mut compose = get_env_string("INPUT_DOCKER_COMPOSE", None);
+    let compose_path = get_env_string("INPUT_DOCKER_COMPOSE_PATH", None);
+    let stack_name = env::var("INPUT_STACKNAME").unwrap();
+    let api_key = get_env_string("INPUT_ACCESS_TOKEN", None);
+    let username = get_env_string("INPUT_USERNAME", None);
+    let password = get_env_string("INPUT_PASSWORD", None);
+    if api_key == "" && (username == "" || password == "") {
+        panic!("api_key and username or password cannot both empty");
+    }
+    let repo_username = get_env_string("INPUT_REPO_USERNAME", None);
+    let repo_password = get_env_string("INPUT_REPO_PASSWORD", None);
+    let images_str = get_env_string("INPUT_IMAGENAMES", None);
+    let variables = get_pair_from_env("INPUT_VARIABLES");
+    let envs = get_pair_from_env("INPUT_ENV");
+    let client = reqwest::Client::new();
+
+    //read content of compose_path to compose
+    if compose == "" && compose_path != "" && variables.len() > 0 {
+        compose = client
+            .get(format!(
+                "{}/raw/branch/{}/{}",
+                env::var("DRONE_GIT_HTTP_URL").unwrap(),
+                env::var("DRONE_REPO_BRANCH").unwrap(),
+                compose_path
+            ))
+            .send()
+            .await?
+            .text()
+            .await?;
+    }
+    //replace variables
+    if variables.len() > 0 && compose != "" {
+        for v in variables {
+            compose = compose.replace(format!("{{{{ {} }}}}", v.name).as_str(), &v.value);
+            compose = compose.replace(format!("{{{{{}}}}}", v.name).as_str(), &v.value);
+        }
+    }
+
+    //1. login to portainer
+    let mut auth_name = "X-API-Key";
+    let mut auth_value = api_key;
+    if auth_value == "" {
+        auth_name = "Authorization";
+        let login_result: serde_json::Value = client
+            .post(format!("{}/api/auth", &server))
+            .json(&serde_json::json!({
+                "Username": &username,
+                "Password": &password,
+            }))
+            .send()
+            .await?
+            .json()
+            .await?;
+        auth_value = format!("Bearer {}", &login_result["jwt"].as_str().unwrap());
+    }
+
+    //2. pull image
+    if images_str != "" {
+        println!("pull images: {}", &images_str);
+        //get all registry
+        let registries: serde_json::Value = client
+            .get(format!("{}/api/registries", &server))
+            .header(auth_name, &auth_value)
+            .send()
+            .await?
+            .json()
+            .await?;
+        let mut registy_map: HashMap<&str, i32> = HashMap::new();
+        for r in registries.as_array().unwrap() {
+            registy_map.insert(r["URL"].as_str().unwrap(), r["Id"].as_i64().unwrap() as i32);
+        }
+
+        let images: Vec<&str> = images_str.split(',').collect();
+        for image in images {
+            let mut pull_image_header = reqwest::header::HeaderMap::new();
+            pull_image_header.insert(auth_name, auth_value.parse().unwrap());
+            let registry_name = image.split('/').nth(0).unwrap();
+            //if image is in registry_map, pull it with X-Registry-Auth
+            if registy_map.contains_key(registry_name) {
+                let registry_id = registy_map[registry_name];
+                let repo_auth = encode(format!("{{\"registryId\":{}}}", registry_id));
+                pull_image_header.insert("X-Registry-Auth", repo_auth.parse().unwrap());
+            }
+            let pull_image_result = client
+                .post(format!(
+                    "{}/api/endpoints/{}/docker/images/create?fromImage={}",
+                    &server,
+                    &endpoint,
+                    image.trim()
+                ))
+                .headers(pull_image_header)
+                .send()
+                .await?;
+            if pull_image_result.status() == 200 {
+                println!("pull image success : {}", image);
+            } else {
+                let msg_detail: serde_json::Value = pull_image_result.json().await?;
+                println!("message:{}", &msg_detail["message"].as_str().unwrap());
+            }
+        }
+    }
+
+    //3. get stack id
+    let stacks: serde_json::Value = client
+        .get(format!(
+            "{}/api/stacks?filters={{\"EndpointID\":{},\"IncludeOrphanedStacks\":true}}",
+            &server, &endpoint
+        ))
+        .header(auth_name, &auth_value)
+        .send()
+        .await?
+        .json()
+        .await?;
+    let mut stack_map: HashMap<&str, i32> = HashMap::new();
+    for s in stacks.as_array().unwrap() {
+        stack_map.insert(
+            s["Name"].as_str().unwrap(),
+            s["Id"].as_i64().unwrap() as i32,
+        );
+    }
+
+    if stack_map.contains_key(stack_name.as_str()) {
+        let stack_id = stack_map[stack_name.as_str()];
+        println!("update stack id: {}", stack_id);
+        // compose is empty, get original stack content
+        if compose == String::default() {
+            let compose_result: serde_json::Value = client
+                .get(format!("{}/api/stacks/{}/file", &server, stack_id))
+                .header(auth_name, &auth_value)
+                .send()
+                .await?
+                .json()
+                .await?;
+            compose = compose_result["StackFileContent"]
+                .as_str()
+                .unwrap()
+                .to_string();
+        }
+        //4. update stack
+        let update_result: serde_json::Value = client
+            .put(format!(
+                "{}/api/stacks/{}?endpointId={}",
+                &server, stack_id, endpoint
+            ))
+            .header(auth_name, auth_value)
+            .json(&serde_json::json!({
+                "id": stack_id,
+                "StackFileContent": &compose,
+                "Env": envs,
+                "Prune": false}))
+            .send()
+            .await?
+            .json()
+            .await?;
+        match update_result["message"].as_str() {
+            Some(msg) => {
+                println!("update stack failed: {}", msg);
+                panic!("update stack failed");
+            }
+            None => println!("update stack success"),
+        }
+        return Ok(());
+    }
+
+    //5. create stack
+    if compose == "" && compose_path == "" {
+        panic!("compose is empty, cannot create stack");
+    }
+    if compose == "" {}
+    //type: 0: docker compose, 1: docker stack
+    //method: file string or repository
+    let create_result: serde_json::Value = match compose.as_str() {
+        "" => {
+            client
+                .post(format!(
+                    "{}/api/stacks?endpointId={}&method=repository&type=2",
+                    &server, endpoint
+                ))
+                .header(auth_name, auth_value)
+                .json(&serde_json::json!({
+                    "repositoryURL": env::var("DRONE_GIT_HTTP_URL").unwrap(),
+                    "repositoryReferenceName": env::var("DRONE_COMMIT_REF").unwrap(),
+                    "composeFile": compose_path,
+                    "repositoryAuthentication": repo_password != "",
+                    "repositoryUsername": repo_username,
+                    "repositoryPassword": repo_password,
+                    "Env": envs,
+                    "Name": &stack_name,
+                }))
+                .send()
+                .await?
+                .json()
+                .await?
+        }
+
+        c => {
+            client
+                .post(format!(
+                    "{}/api/stacks?endpointId={}&method=string&type=2",
+                    &server, endpoint
+                ))
+                .header(auth_name, auth_value)
+                .json(&serde_json::json!({
+                    "StackFileContent": c,
+                    "Env": envs,
+                    "Name": &stack_name,
+                }))
+                .send()
+                .await?
+                .json()
+                .await?
+        }
+    };
+    match create_result["message"].as_str() {
+        Some(msg) => {
+            println!("create stack failed: {}", msg);
+            panic!("create stack failed");
+        }
+        None => println!("create stack success"),
+    }
+
+    Ok(())
+}
