94e1d521c6d56aeb2cad87dbf000a6f694906a53
LiGuo <liguo1976@outlook.com>
2023-04-15 12:47:16 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -242,17 +242,17 @@ async fn main() -> Result<(), reqwest::Error> {
     //type: 0: docker compose, 1: docker stack
     //method: file string or repository
 
-    // let create_json = &serde_json::json!({
-    //     "repositoryURL": format!("https://github.com/{}",env::var("GITHUB_REPOSITORY").unwrap()),
-    //     "repositoryReferenceName": env::var("GITHUB_REF").unwrap(),
-    //     "composeFile": compose_path,
-    //     // "repositoryAuthentication": repo_password != "",
-    //     "repositoryUsername": repo_username,
-    //     "repositoryPassword": repo_password,
-    //     "Env": envs,
-    //     "Name": &stack_name,
-    // });
-    // println!("{}", &create_json);
+    let create_json = &serde_json::json!({
+        "repositoryAuthentication": repo_password != "",
+        "repositoryUsername": repo_username,
+        "repositoryPassword": repo_password,
+        "repositoryURL": format!("https://github.com/{}",env::var("GITHUB_REPOSITORY").unwrap()),
+        "repositoryReferenceName": env::var("GITHUB_REF").unwrap(),
+        "composeFile": compose_path,
+        "Env": envs,
+        "Name": &stack_name,
+    });
+    println!("{}", &create_json);
     let create_result: serde_json::Value = match compose.as_str() {
         "" => {
             client
@@ -261,16 +261,7 @@ async fn main() -> Result<(), reqwest::Error> {
                     &server, endpoint
                 ))
                 .header(auth_name, auth_value)
-                .json(&serde_json::json!({
-                    "repositoryAuthentication": repo_password != "",
-                    "repositoryUsername": repo_username,
-                    "repositoryPassword": repo_password,
-                    "repositoryURL": format!("https://github.com/{}",env::var("GITHUB_REPOSITORY").unwrap()),
-                    "repositoryReferenceName": env::var("GITHUB_REF").unwrap(),
-                    "composeFile": compose_path,
-                    "Env": envs,
-                    "Name": &stack_name,
-                }))
+                .json(create_json)
                 .send()
                 .await?
                 .json()
